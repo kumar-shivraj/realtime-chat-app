0aa3dbd8550db623d6a22e73cc5fbac5bb225b93
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-25 07:20:57 +0530

    realtime-chat-app: create the different routes for the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import ProfilePage from "./pages/ProfilePage.jsx"
+
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
