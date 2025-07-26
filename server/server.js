2a8fe4fe80a8c1747ded4e292dcc429cad90c7b1
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-26 12:32:29 +0530

    realtime-chat-app: initialize the socket server

--- server/server.js
+++ server/server.js
@@ -5,10 +5,40 @@ import http from 'http'
 import connectDB from './lib/db.js'
 import userRouter from './routes/user.route.js'
 import messageRouter from './routes/message.route.js'
+import { Server } from 'socket.io'
 
 // create express app using HTTP server, because socket.io needs an HTTP server
 const app = express()
 const server = http.createServer(app)
+
+// initialize socket.io server
+export const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+})
+
+// store online users
+export const userSocketMap = {} // {userId: socketId}
+
+// socket.io connection handler
+io.on('connection', (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log('A user connected:', socket.id)
+
+  if(userId) 
+      userSocketMap[userId] = socket.id;
+
+  // emit online users to all connected clients
+  io.emit('getOnlineUsers', Object.keys(userSocketMap))
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id)
+    delete userSocketMap[userId]
+    io.emit('getOnlineUsers', Object.keys(userSocketMap))
+  })
+
+})
 
 // middleware setup
 app.use(express.json({ limit: '4mb' }))
