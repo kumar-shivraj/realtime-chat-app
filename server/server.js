3ca0e311b56ce3dc263023d8c95e5d1ac438c95c
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-26 07:48:55 +0530

    realtime-chat-app: create the server and connect to the database

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from 'http';
+import { connect } from 'http2';
+import connectDB from './lib/db.js';
+
+
+// create express app using HTTP server, because socket.io needs an HTTP server
+const app = express();
+const server = http.createServer(app);
+
+// middleware setup
+app.use(express.json({limit: '4mb'}));
+app.use(cors());
+
+
+app.use("/api/status", (req, res) => {
+    res.status(200).json({status: "Server is live"});
+});
+
+// connect to MongoDB
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
