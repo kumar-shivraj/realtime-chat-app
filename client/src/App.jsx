c81c974c35199a65cfc5c2fb0f5ccdce62d12ae6
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-26 14:15:16 +0530

    add AuthContext with login, logout, profile update and socket integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <div className="bg-[url('./assets/bgImage.svg')] bg-contain">
+      <Toaster />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
