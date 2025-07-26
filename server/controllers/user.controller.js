735a02ddd373ef298e5371ab1027b9016bada574
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-26 16:56:17 +0530

    realtime-chat-app: bug fix in order to start the application properly

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
-import cloudinary from '../lib/cloudinary'
-import { generateToken } from '../lib/utils'
-import User from '../models/User.model'
+import cloudinary from '../lib/cloudinary.js'
+import { generateToken } from '../lib/utils.js'
 import bcrypt from 'bcryptjs'
+import User from '../models/User.model.js'
 
 // signup a new user
 export const signup = async (req, res) => {
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { checkAuth, login, signup, updateProfile } from '../controllers/user.controller'
-import { protectRoute } from '../middleware/auth'
+import { checkAuth, login, signup, updateProfile } from '../controllers/user.controller.js'
+import { protectRoute } from '../middleware/auth.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/signup', signup)
 userRouter.post('/login', login)
 userRouter.put('/update-profile', protectRoute, updateProfile)
-userRouter.put('/check', protectRoute, checkAuth)
+userRouter.get('/check', protectRoute, checkAuth)
 
 export default userRouter
