a13539cb1bd25b3c2a301bfa4f9e41cb91d72efe
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-28 07:13:27 +0530

    realtime-chat-app: implement the right sidebar to display the selected user, media and other things

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,15 +1,29 @@
-import assets, { imagesDummyData } from '../assets/assets'
-const RightSidebar = ({ selectedUser }) => {
+import { useContext, useEffect, useState } from 'react'
+import { ChatContext } from '../../context/ChatContext'
+import assets from '../assets/assets'
+import { AuthContext } from '../../context/AuthContext'
+const RightSidebar = () => {
+  const { selectedUser, messages } = useContext(ChatContext)
+  const { logout, onlineUsers } = useContext(AuthContext)
+  const [msgImages, setMsgImages] = useState([])
+
+  // get all the images from the messages and set them to state
+  useEffect(() => {
+    const images = messages.filter(msg => msg.image).map(msg => msg.image)
+    setMsgImages(images)
+  }, [messages])
+
   return (
     selectedUser && (
       <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? 'max-md:hidden' : ''}`}>
         <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
           <img
-            src={selectedUser?.profilePic || assets.avatarIcon} alt=''
+            src={selectedUser?.profilePic || assets.avatarIcon}
+            alt=''
             className='w-20 aspect-[1/1] rounded-full'
           />
           <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-            <p className='w-2 h-2 rounded-full bg-green-500' />
+            {onlineUsers.includes(selectedUser._id) && <p className='w-2 h-2 rounded-full bg-green-500' />}
             {selectedUser?.fullName}
           </h1>
           <p className='px-10 mx-auto'>
@@ -23,7 +37,7 @@ const RightSidebar = ({ selectedUser }) => {
           <p>Media</p>
           <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
             {
-              imagesDummyData.map((url, index) => (
+              msgImages.map((url, index) => (
                 <div
                   key={index}
                   onClick={() => window.open(url)}
@@ -36,7 +50,10 @@ const RightSidebar = ({ selectedUser }) => {
           </div>
         </div>
 
-        <button className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'>
+        <button
+          className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer'
+          onClick={() => logout()}
+        >
           Logout
         </button>
       </div>
