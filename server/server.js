dcf4004921bd883c96e5f7e5013657985cdf6064
kumar-shivraj <shivrajkumar11math@gmail.com>
2025-07-28 09:24:21 +0530

    realtime-chat-app: fix for deployment on vercel

--- server/server.js
+++ server/server.js
@@ -52,7 +52,12 @@ app.use('/api/messages', messageRouter)
 // connect to MongoDB
 await connectDB()
 
-const PORT = process.env.PORT || 5000
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
+}
+
+// export server for vercel
+export default server
